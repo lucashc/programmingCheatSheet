eaded9d13e6a0c1013bc7bad5d87e4ca9cb09dbd
Sven Holtrop <yadeses@gmail.com>
2019-09-17 09:01:20 +0200

    Topo sort

---
+++ algorithms/topological_sort.py
@@ -0,0 +1,22 @@
+def topological_sort(graph):
+    from collections import deque
+    indeg = [0] * len(graph)
+    result = []
+    q = deque()
+    for ns in graph:
+        for n in ns:
+            indeg[n] += 1
+
+    for i in range(len(graph)):
+        if indeg[i] == 0:
+            q.appendleft(i)
+
+    while q:
+        n = q.pop()
+        result.append(n)
+        for i in graph[n]:
+            indeg[i] -= 1
+            if indeg[i] == 0:
+                q.appendleft(i)
+    return result
+
