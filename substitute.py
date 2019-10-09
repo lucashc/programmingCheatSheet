466a79cc3f017e3c2dd2d3f783033b83a5ae114e
BuildTools <lucascrijns@gmail.com>
2019-10-10 00:46:21 +0200

    Added titlepage and intersection algorithm

--- substitute.py
+++ substitute.py
@@ -1,22 +1,22 @@
 #!/bin/env python
 import sys
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, join, getsize
 
 # Get files in directory
 algorithm_dir = sys.argv[1]
-filelist = [f for f in listdir(algorithm_dir) if isfile(join(algorithm_dir, f))]
-filelist.sort()
+filelist = [join(algorithm_dir, f) for f in listdir(algorithm_dir) if isfile(join(algorithm_dir, f))]
+filelist.sort(key=lambda x: getsize(x))
 
 def get_lang(f):
-    if f[-3:] == ".py": return "python"
-    elif f[-5:] == ".java": return "java"
-    elif f[-4:] == ".cpp": return "cpp"
+    if f[-3:] == ".py": return "Python"
+    elif f[-5:] == ".java": return "Java"
+    elif f[-4:] == ".cpp": return "C++"
     else: return "java"
 
 inclusion = ""
 for f in filelist:
-    with open(join(algorithm_dir, f)) as file:
+    with open(f) as file:
             data = file.read()
             code = "\n".join([i for i in data.partition('\n')[-1].splitlines() if not i.startswith("##")])
             title = data.splitlines()[0][2:].strip()
